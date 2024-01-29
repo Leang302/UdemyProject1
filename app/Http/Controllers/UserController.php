37d302594446984974a72f142f2a892a6e3949f2
kimleang <leangk49@gmail.com>
2024-01-29 22:32:23 +0700

    login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,38 @@ use Psy\CodeCleaner\PassableByReferencePass;
 
 class UserController extends Controller
 {
+    public function showCorrectHomePage()
+    {
+        if (auth()->check()) {
+            return view('homepage-feed');
+
+        } else {
+            return view('homepage');
+        }
+    }
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            "loginusername" => "required",
+            'loginpassword' => "required"
+        ]);
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return 'congrats';
+        } else {
+            return 'fails';
+        }
+    }
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
             "username" => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
             "email" => ['required', 'email', Rule::unique("users", 'email')],
-            "password" => ["required", 'min:6', 'max:30', 'confirmed'],
+            "password" => ["required", 'min:3', 'max:30', 'confirmed'],
         ]);
         $incomingFields['password'] = bcrypt($incomingFields['password']);
         User::create($incomingFields);
         return "Hello from register functon";
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ExampleController::class, 'homePage']);
+Route::get('/', [UserController::class, 'showCorrectHomePage']);
 Route::get('/about', [ExampleController::class, 'aboutPage']);
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login',[UserController::class,'login']);
