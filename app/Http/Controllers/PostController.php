82fcd0c777454f70ee7f47a68774f0f2ba21a09a
kimleang <leangk49@gmail.com>
2024-01-30 18:31:17 +0700

    markdown text

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
     public function viewSinglePost(Post $post)
     {
+        $post['body']= strip_tags(Str::markdown($post->body),'<p><ul><li><ol>');
         return view('single-post', ['post' => $post]);
     }
     public function storeNewPost(Request $request)
